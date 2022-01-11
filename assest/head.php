dca9c7c720a25a6671787e43c48afdc7b1bd83d4
SteveOwago <33925746+SteveOwago@users.noreply.github.com>
2022-01-11 21:16:22 +0300

    session changes

--- assest/head.php
+++ assest/head.php
@@ -3,11 +3,12 @@
 <?php
 // Initialize the session
 session_start();
-$loggedin = false;
 
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
    $loggedin = true;
+}else{
+   $loggedin = false;
 }
 
 ?>
--- login.php
+++ login.php
@@ -53,7 +53,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                             session_start();
 
                             // Store data in session variables
-                            $_SESSION["loggedin"] = true;
+                            $_SESSION["loggedin"] == true;
                             $_SESSION["id"] = $id;
                             $_SESSION["username"] = $username;
 
